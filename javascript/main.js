e3c386701e058084d1755725dde2e7ece3eb9ebe
Sirjamo1 <jam_how@hotmail.com>
2023-03-09 12:11:58 +1000

    simplified js

--- javascript/main.js
+++ javascript/main.js
@@ -3,12 +3,16 @@ const menuButton = document.getElementById("menu-button");
 const toggleNavDisplay = () => {
     navList[0].classList.toggle("show-mobile-nav");
 };
-menuButton.addEventListener("click", () => toggleNavDisplay());
+menuButton.addEventListener("click", toggleNavDisplay);
 [...navList[0].children].forEach((li) => {
-    li.addEventListener("click", () => toggleNavDisplay());
+    li.addEventListener("click", toggleNavDisplay);
 });
-window.addEventListener("resize", (e) => {
-    if (document.body.clientWidth >= 500 && navList[0].classList.length > 0) {
+//Remove show-mobile-nav class (if it was open when resizing screen)
+window.addEventListener("resize", () => {
+    if (
+        document.body.clientWidth >= 500 &&
+        navList[0].contains("show-mobile-nav")
+    ) {
         navList[0].classList.remove("show-mobile-nav");
     }
 });
