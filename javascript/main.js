4dc7727d535f8d01233f3a24400ebfc15516ab97
Sirjamo1 <jam_how@hotmail.com>
2023-03-09 10:44:09 +1000

    moved all onclick funcs to js file

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,14 @@
 const navList = document.getElementsByClassName("nav-list");
+const menuButton = document.getElementById("menu-button");
 const toggleNavDisplay = () => {
-    navList[0].classList.toggle("mobile-nav-active");
+    navList[0].classList.toggle("show-mobile-nav");
 };
+menuButton.addEventListener("click", () => toggleNavDisplay());
+[...navList[0].children].forEach((li) => {
+    li.addEventListener("click", () => toggleNavDisplay());
+});
 window.addEventListener("resize", (e) => {
     if (document.body.clientWidth >= 500 && navList[0].classList.length > 0) {
-        navList[0].classList.remove("mobile-nav-active")
+        navList[0].classList.remove("show-mobile-nav");
     }
 });
